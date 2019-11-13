fb3a1bc9b63dd5fb1e62cfc6c9efba5d4ad08388
Connie Guan <connieguan123@gmail.com>
2019-11-12 19:31:54 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern void printfib(int f);
+extern int asm_fib(int f);
+
+int main(int argc, char **argv){
+
+  int f = strtol(argv[1], NULL, 10);
+  int fib = asm_fib(f);
+
+  printf("The Fibonacci number of %d is %d\n", f, fib);
+}
